e687c7f4b3d4cf024b991a2639c446c7ed139911
erikyoung <erik.young@rmit.edu.vn>
2016-10-04 01:33:17 +0700

    more security

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.paginate(:page => params[:page], per_page: 2)
@@ -20,8 +20,7 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def edit
     @place = Place.find(params[:id])
-
-    if @place.user!= current_user
+    if @place.user != current_user
       return render text: 'Not Allowed', status: :forbidden
     end
   end
@@ -34,6 +33,9 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render text: 'Not Allowed', status: :forbidden
+    end
     @place.destroy
     redirect_to root_path
   end
