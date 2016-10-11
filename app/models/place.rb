333854ca0c81211f3001a7f76b9909b953102afc
erikyoung <erik.young@rmit.edu.vn>
2016-10-11 23:40:08 +0700

    added database connections lesson 31

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,11 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+
+
   validates :name, :length => { minimum: 4 }, :presence => true
   validates :address, presence: true
   validates :description, presence: true
